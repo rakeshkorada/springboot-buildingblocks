7004f81337b7891dae2fde59881bc4803def5dd2
rakeshk <rkorada@tracfone.com>
2019-11-16 15:37:34 +0530

    JPA Based RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'kreddy@stacksimlify.com', 'kalyan', 'Reddy', 'admin', 'ssn101', 'kreddy');
+insert into user values(102, 'gwiser@stacksimlify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser');
+insert into user values(103, 'dmark@stacksimlify.com', 'David', 'mark', 'admin', 'ssn103', 'dmark');

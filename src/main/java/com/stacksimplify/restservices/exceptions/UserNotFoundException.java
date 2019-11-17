6350c4cffd350fee70b40323e5a168d878d79d53
rakeshk <rkorada@tracfone.com>
2019-11-17 09:57:59 +0530

    ResponseStatusException First Commit

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserNotFoundException.java
@@ -0,0 +1,17 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
